2c5c20b076271eeb9d5b74e9532f5bb04de971bb
Ábel <fenykep@Fodor-MBP-2.fritz.box>
2023-03-28 16:08:41 +0200

    mukszik

--- res/compile.sh
+++ res/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pug index.pug events.pug exibs.pug --out html/hu/
+pug index.pug events.pug exibs.pug --out ../page/
 #pug indexen.pug eventsen.pug contacten.pug exibsen.pug artistsen.pug abouten.pug --out html/en/
