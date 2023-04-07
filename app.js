5cde7a2318cb69be935ff22938574c32adebf165
Ábel <abel@fenykep>
2023-04-07 15:08:34 +0200

    db insert works (only title,date json)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -12,6 +13,7 @@ app.use(express.static('page'))
 app.use('../page/img', express.static('page/img'))
 app.use('../page/css', express.static('page/css'))
 
+app.use(bodyParser.json())
 
 app.set('views', './res') // set the views directory
 app.set('view engine', 'pug')
@@ -20,10 +22,10 @@ app.set('view engine', 'pug')
 mongoose.connect('mongodb://db:27017/galeriDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+})
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const entrySchema = new mongoose.Schema({
   id: Number,
@@ -33,10 +35,10 @@ const entrySchema = new mongoose.Schema({
   tags: [String],
   directory: String,
   numImages: Number,
-  isExib: Boolean
-});
+  isEvent: Boolean
+})
 
-const EntryModel = mongoose.model('Entry', entrySchema);
+const EntryModel = mongoose.model('Entry', entrySchema)
 
 
 
@@ -61,22 +63,56 @@ app.get('/events', async (req, res) => {
   // const cardData = await getCardData(); // retrieve card data from MongoDB
   // res.render('menu', { title: cardData.title }); // pass title data to Pug template
   res.render('eventsMenu', { title: "Bolombér" })
-});
+})
 
 app.get('/exibs', async (req, res) => {
-    const entry = await EntryModel.find()
+  const entry = await EntryModel.find()
 
   res.render('exibsMenu', { data: entry })
-});
+})
 
 app.get('/admin', (req, res) => {
     res.sendFile(__dirname + '/page/admin.html')
   // res.sendFile('admin.html')
 })
 
+// const entries = [
+//   {
+//     id: 4,
+//     title: 'Exhibition 1',
+//     artist: 'Artist 1',
+//     date: new Date('2023-04-01'),
+//     tags: ['painting', 'sculpture'],
+//     directory: 'exhibition1',
+//     numImages: 10,
+//     isEvent: false
+//   }
+// ]
+
+async function insertEntries(entries) {
+  // const result = await db.collection('entries').insertMany(entries);
+  const result = await db.collection('entries').insertOne(entries)
+  console.log(`${result.insertedCount} entries inserted`)
+}
+
 app.post('/uploadEvent', (req, res) => {
-  res.send('Got a POST request')
-})
+  // const { title, date } = req.body
+  console.log(req.body)
+  // insertEntries(entries);
+  const newEntry = new EntryModel({
+    id: 9, // auto-increment id logic goes here
+    title: req.body.title,
+    artist: 'Artist 1',
+    date: new Date(req.body.date),
+    tags: ['painting', 'sculpture'],
+    directory: 'exibs/exhibition1',
+    numImages: 4,
+    isEvent: true
+  })
+  console.log(newEntry)
+  insertEntries(newEntry)
+  res.send({ success: true })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
